fd67e2ad307b8c88a8415ec0e4d4806eae8f1913
LitviVit <Litv_jn71@student.itstep.org>
2024-07-25 00:04:13 +0300

    product card prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
+import productsFromServer from './api/products.json';
+import { ProductCard } from './components/ProductCard/ProductCard';
+import { Product } from './types';
+
+const products: Product[] = productsFromServer;
 
 export function App() {
   return (
     <>
       <h1>phone catalog</h1>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+        {products.map(product => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
     </>
   );
 }
