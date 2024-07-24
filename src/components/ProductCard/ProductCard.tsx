c0f52f51664f8fd93f5bf977a8cadb184aef5677
LitviVit <Litv_jn71@student.itstep.org>
2024-07-25 00:14:37 +0300

    cleanup

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -7,31 +7,6 @@ type Props = {
 
 export const ProductCard: React.FC<Props> = ({ product }) => {
   return (
-    // <div className="w-[272px] h-[506px] bg-white shadow-md md:max-w-2xl">
-    //   <div className="">
-    //     <div className="md:flex-shrink-0">
-    //       <img
-    //         className="h-48 w-full object-cover md:w-48"
-    //         src={product.image}
-    //         alt="Product Image"
-    //       />
-    //     </div>
-    //     <div className="p-8">
-    //       <h1 className="block mt-1 font-mont text-sm leading-[21px] text-[#313237] text-left">
-    //         {product.name}
-    //       </h1>
-    //       <div className="mt-4 flex items-center justify-between">
-    //         <span className="text-lg font-semibold text-gray-900">
-    //           ${product.price}
-    //         </span>
-    //         <button className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600">
-    //           Add to Cart
-    //         </button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="w-72 bg-white shadow-lg rounded-lg p-8 border border-gray-200">
       <img
         className="w-full h-56 object-contain rounded-t-lg"
@@ -62,7 +37,7 @@ export const ProductCard: React.FC<Props> = ({ product }) => {
           </div>
         </div>
         <div className="mt-6 flex justify-between">
-          <button className="flex items-center justify-center h-10 w-40 bg-black text-white rounded-none hover:bg-gray-800 transition">
+          <button className="flex items-center justify-center h-10 w-40 bg-[#313237] text-white rounded-none hover:bg-gray-800 transition">
             Add to cart
           </button>
           <button className="h-10 w-14 hover:text-gray-800 bg-transparent border border-gray-500 rounded-none">
