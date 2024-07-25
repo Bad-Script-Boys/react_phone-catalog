dd2e4d51a6963ceeb2db2dde69011b678515a5ff
LitviVit <Litv_jn71@student.itstep.org>
2024-07-25 11:49:29 +0300

    branch

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ export function App() {
   return (
     <>
       <ThemeProvider>
-      <Header />
-    </ThemeProvider>
+        <Header />
+      </ThemeProvider>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
         {products.map(product => (
           <ProductCard product={product} key={product.id} />
