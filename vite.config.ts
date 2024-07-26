531818b946bd6c667530a4e3034ddba5c755c834
Avramidi Yaroslav <passenger1177@gmail.com>
2024-07-26 16:51:32 +0300

    prepared deploy

--- vite.config.ts
+++ vite.config.ts
@@ -12,13 +12,23 @@ import react from '@vitejs/plugin-react';
 //       ? '/react_phone_catalog/'
 //       : '/',
 // });
-export default defineConfig(({ command }) => ({
+// export default defineConfig(({ command }) => ({
+//   plugins: [react()],
+//   base:
+//     command === 'build' && process.env.DEPLOY_ENV === 'github'
+//       ? '/react_phone-catalog/'
+//       : '/',
+//   server: {
+//     open: true,
+//   },
+// }));
+
+// import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/react_phone-catalog/',
   plugins: [react()],
-  base:
-    command === 'build' && process.env.DEPLOY_ENV === 'github'
-      ? '/react_phone-catalog/'
-      : '/',
   server: {
     open: true,
   },
-}));
+});
