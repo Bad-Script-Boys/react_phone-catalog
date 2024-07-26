cf6e5c5e3f96d6f8d64ace439c02ebba2f4a6ec9
Stanislav Kushnir <168120297+StsKushnir@users.noreply.github.com>
2024-07-26 13:58:30 +0300

    Create footer (#6)
    * add created footer
    
    * made changes
    
    ---------
    
    Co-authored-by: Yaroslav Avramidi <161472089+yaroslav1177@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header/Header';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { Footer } from './components/footer/Footer';
 
 export function App() {
   return (
     <>
       <ThemeProvider>
         <Header />
+        <Footer />
       </ThemeProvider>
     </>
   );
