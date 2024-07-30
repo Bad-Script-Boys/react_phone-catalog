aac0024d01f6c009422aee8f4cb2b2610fb61efd
Stanislav Kushnir <florian.of.lorch@gmail.com>
2024-07-30 12:39:15 +0300

    solve problems with crlf

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import { Header } from './components/Header/Header';
 import { Footer } from './components/footer/Footer';
 import { Outlet } from 'react-router-dom';
 import { AboutSection } from './components/About';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 export function App() {
   return (
-    <>
-      <ThemeProvider>
-        <Header />
-        <Outlet />
-        <AboutSection />
-        <Footer />
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <Header />
+      <Outlet />
+      <AboutSection />
+      <Footer />
+    </ThemeProvider>
   );
 }
-
 
