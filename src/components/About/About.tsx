64b850ecae5fcbed14e39fb3a77c7ee85714fbb7
Vitalii <168312293+LitviVit@users.noreply.github.com>
2024-08-01 13:11:42 +0300

    add breadcrumbs (#20)

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './About.module.scss';
-import { Breadcrumbs } from '../Breadcrumbs';
 import { Gallery } from '../Gallery';
 import { Categories } from '../Categories';
 import { Description } from '../Description';
 import { Specifications } from '../Specifications';
+import { Breadcrumbs } from '../Breadcrumbs';
 
 const {
   about,
@@ -18,8 +18,9 @@ export const AboutSection: React.FC = () => {
   return (
     <div className={about}>
       <div className={about__content}>
-        <Breadcrumbs />
-
+        <div className="mt-20">
+          <Breadcrumbs />
+        </div>
         <div className={about__wrap}>
           <h1 className={about__section_title}>
             Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)
--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,43 +1,61 @@
-import styles from './Breadcrumbs.module.scss';
-import { LuHome } from 'react-icons/lu';
-import { GoChevronRight } from 'react-icons/go';
-import { GoChevronLeft } from 'react-icons/go';
 import React from 'react';
-
-const {
-  breadcrumbs,
-  breadcrumbs__wrap,
-  breadcrumbs__home_link,
-  breadcrumbs__type_link,
-  breadcrumbs__device,
-  breadcrumbs__wrap_btn,
-  breadcrumbs__btn,
-} = styles;
+import { Link, useLocation } from 'react-router-dom';
+import { HomeIcon } from '../ThemeIcons/HomeIcon';
+import { useTheme } from '../../contexts/ThemeContext';
 
 export const Breadcrumbs: React.FC = () => {
+  const location = useLocation();
+  const pathnames = location.pathname.split('/').filter(x => x);
+  const { theme } = useTheme();
+
   return (
-    <div className={breadcrumbs}>
-      <div className={breadcrumbs__wrap}>
-        <a href="/" className={breadcrumbs__home_link}>
-          <LuHome className="w-4 h-4 " />
-        </a>
-        <span>
-          <GoChevronRight className="w-4 h-4 text-[#B4BDC3]" />
-        </span>
-        <a href="/phones" className={breadcrumbs__type_link}>
-          {`${'Phones'}`}
-        </a>
-        <span>
-          <GoChevronRight className="w-4 h-4 text-[#B4BDC3]" />
-        </span>
-        <p className={breadcrumbs__device}>
-          {`${'Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)'}`}
-        </p>
-      </div>
-      <div className={breadcrumbs__wrap_btn}>
-        <GoChevronLeft className="w-4 h-4 text-[#B4BDC3]" />
-        <button className={breadcrumbs__btn}>Back</button>
-      </div>
-    </div>
+    <nav className="flex mb-6" aria-label="Breadcrumb">
+      <ol className="inline-flex items-center space-x-1 md:space-x-3">
+        <li className="inline-flex items-center">
+          <Link
+            to="/"
+            className={`text-${theme === 'dark' ? '[#F1F2F9] hover:text-gray-200' : 'gray-700 hover:text-gray-900'} inline-flex items-center`}
+          >
+            <HomeIcon
+              fill={`${theme === 'dark' ? '#F1F2F9' : '#313237'}`}
+              className="h-4 w-4 mb-1 mr-2"
+            />
+            Home
+          </Link>
+        </li>
+        {pathnames.map((value, index) => {
+          const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+          const isLast = index === pathnames.length - 1;
+          return (
+            <li key={to} className="inline-flex items-center">
+              <div className="flex items-center">
+                <svg
+                  className="w-6 h-6 text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7.293 14.707a1 1 0 0 1 0-1.414L10.586 10 7.293 6.707a1 1 0 0 1 1.414-1.414l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414 0z" />
+                </svg>
+                {isLast ? (
+                  <span
+                    className={`ml-1 text-sm font-medium text-${theme === 'dark' ? '[#75767F]' : 'gray-700'}  md:ml-2 capitalize`}
+                  >
+                    {value}
+                  </span>
+                ) : (
+                  <Link
+                    to={to}
+                    className={`ml-1 text-sm font-medium text-${theme === 'dark' ? '[#F1F2F9] hover:text-gray-200' : 'gray-700 hover:text-gray-900'} md:ml-2 capitalize`}
+                  >
+                    {value}
+                  </Link>
+                )}
+              </div>
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
   );
 };
