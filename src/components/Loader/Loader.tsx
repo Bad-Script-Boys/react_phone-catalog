41e396d8ddb0fed7f9c8fa19c152b12c44ece27b
Avramidi Yaroslav <passenger1177@gmail.com>
2024-08-03 03:51:51 +0300

    fix loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -9,30 +9,31 @@
 // };
 
 import styles from './Loader.module.scss';
+import { useTheme } from '../../contexts/ThemeContext';
 
 export const Loader = () => {
+  const { theme } = useTheme();
+
   return (
-    <>
-      <div className={styles.loaderWrapper}>
-        <div className={styles.body}>
-          <span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-          <div className={styles.base}>
-            <span></span>
-            <div className={styles.face}></div>
-          </div>
-        </div>
-        <div className={styles.longfazers}>
+    <div className={`${styles.loaderWrapper} ${styles[theme]}`}>
+      <div className={styles.body}>
+        <span>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
+        </span>
+        <div className={styles.base}>
+          <span></span>
+          <div className={styles.face}></div>
         </div>
       </div>
-    </>
+      <div className={styles.longfazers}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
   );
 };
