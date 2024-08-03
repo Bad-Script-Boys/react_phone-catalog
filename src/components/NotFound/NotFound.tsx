3809e1b905f51257eb2ed45d0d17e5f6a96591e4
Avramidi Yaroslav <passenger1177@gmail.com>
2024-08-03 16:32:29 +0300

    change not found page

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,22 +1,49 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
+import styles from './NotFound.module.scss';
+import astronautImage from '../../../public/img/astro.png';
+import { useTheme } from '../../contexts/ThemeContext';
 
-const NotFound: React.FC = () => {
+const NotFound = () => {
+  const { theme } = useTheme();
+  const starsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const createStars = () => {
+      if (starsRef.current) {
+        const starsContainer = starsRef.current;
+        starsContainer.innerHTML = '';
+
+        for (let i = 0; i < 400; i++) {
+          const star = document.createElement('div');
+          star.className = styles.star;
+          star.style.top = `${Math.random() * 200}vh`;
+          star.style.left = `${Math.random() * 100}vw`;
+          starsContainer.appendChild(star);
+        }
+      }
+    };
+
+    createStars();
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4">
-      <div className="mt-[100px] lg:px-[152px]"></div>
-      <h1 className="text-6xl font-bold">404</h1>
-      <img
-        src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
-        alt="404 - Сторінку не знайдено."
-        className=" h-[300px] object-contain"
-      />
-      <p className="text-lg">
-        Look like you&#39;re lost the page you are looking for not avaible!
-      </p>
-      <a
-        className="bg-[#313237] py-4 px-8 text-white hover:scale-110 transition-transform duration-500 no-underline hover:text-white mt-4"
-        href="#"
-      >
+    <div className={`${styles.notFoundWrapper} ${styles[theme]}`}>
+      <div className={styles.starsContainer}>
+        <div
+          className={`${styles.stars} ${styles.starsMoving}`}
+          ref={starsRef}
+        ></div>
+        <div
+          className={`${styles.stars} ${styles.starsMoving}`}
+          style={{ top: '100%' }}
+        ></div>
+      </div>
+      <div
+        className={styles.astronaut}
+        style={{ backgroundImage: `url(${astronautImage})` }}
+      ></div>
+      <h1 className={styles.title}>Page Not Found</h1>
+      <a className={styles.button} href="#">
         Go to Home
       </a>
     </div>
--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './notification.scss';
+import { useTheme } from '../../contexts/ThemeContext';
 
 interface NotificationProviderProps {
   children: ReactNode;
@@ -9,8 +11,10 @@ interface NotificationProviderProps {
 const NotificationProvider: React.FC<NotificationProviderProps> = ({
   children,
 }) => {
+  const { theme } = useTheme();
+
   return (
-    <>
+    <div className={theme === 'dark' ? 'dark-theme' : 'light-theme'}>
       <ToastContainer
         position="bottom-right"
         autoClose={3000}
@@ -21,10 +25,9 @@ const NotificationProvider: React.FC<NotificationProviderProps> = ({
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="colored"
       />
       {children}
-    </>
+    </div>
   );
 };
 
