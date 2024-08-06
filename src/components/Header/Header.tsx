c75ed96472ec5090f3dea0b6fca41009a3b4857f
dmitriy.boboshko <dmitriy.boboshko@vegatele.com>
2024-08-06 04:09:58 +0300

    search fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
+import React, { useState, useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { ThemeSwitch } from '../ThemeSwitch/ThemeSwitch';
-
-import { useContext, useState } from 'react';
 import { Menu } from '../Menu/Menu';
 import { StateContext } from '../../Store';
 import { BasketLogo } from '../ThemeIcons/BasketLogo';
@@ -46,12 +45,19 @@ export const Header: React.FC = () => {
   const { favorites, basket } = state;
   const { theme } = useTheme();
   const [results, setResults] = useState<Product[]>([]);
+  const [focused, setFocused] = useState(false);
 
   let totalQuantity = 0;
 
   for (const item of basket) {
     totalQuantity += item.quantity;
   }
+
+  const handleResultClick = (product: Product) => {
+    setInput('');
+    setResults([]);
+    setFocused(false);
+  };
 
   return (
     <>
@@ -90,9 +96,12 @@ export const Header: React.FC = () => {
         </div>
         <div className="hidden h-16 sm:flex items-center justify-between mx-4 pl-3 dark:border-gray-500 border-[#E2E6E9]">
           <div className="hidden mt-auto xl:block p-4 max-w-[300px] mx-0">
-            <SearchBar setResults={setResults} />
-            {results && results.length > 0 && (
-              <SearchResultsList results={results} />
+            <SearchBar setResults={setResults} setFocused={setFocused} />
+            {focused && results.length > 0 && (
+              <SearchResultsList
+                results={results}
+                onResultClick={handleResultClick}
+              />
             )}
           </div>
           <div className="flex items-center justify-center p-4 gap-4 dark:border-gray-500 border-[#E2E6E9] relative cursor-pointer">
@@ -101,12 +110,12 @@ export const Header: React.FC = () => {
               <FavoriteLogo />
             </NavLink>
             {favorites.length !== 0 && (
-              <span className="flex items-center justify-center text-xs absolute top-3 left-7 h-4 w-4 bg-red-500 text-white rounded-full">
+              <span className="flex items-center justify-center text-xs absolute top-3 right-3 h-4 w-4 bg-red-500 text-white rounded-full">
                 {favorites.length}
               </span>
             )}
           </div>
-          <div className="flex items-center justify-center  dark:border-gray-500 border-[#E2E6E9] h-16 p-4  relative cursor-pointer">
+          <div className="flex items-center justify-center dark:border-gray-500 border-[#E2E6E9] h-16 p-4 relative cursor-pointer">
             <NavLink to="/cart" className={classActiveNavIcon}>
               <BasketLogo />
             </NavLink>
--- src/components/Search/SearchResultList.tsx
+++ src/components/Search/SearchResultList.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import { SearchResult } from './SearchResult';
 import { Product } from '../../types';
 import { useTheme } from '../../contexts/ThemeContext';
+import { Link } from 'react-router-dom';
 
 interface SearchResultsListProps {
   results: Product[];
+  onResultClick: (product: Product) => void;
 }
 
 export const SearchResultsList: React.FC<SearchResultsListProps> = ({
   results,
+  onResultClick,
 }) => {
   const { theme } = useTheme();
 
+  const handleItemClick = (product: Product, event: React.MouseEvent) => {
+    event.stopPropagation();
+    onResultClick(product);
+  };
+
   return (
-    <div className="w-full flex flex-col shadow-lg rounded-lg mt-4 pl-4 max-h-[50vh] overflow-y-auto">
-      <h1 className={`${theme === 'light' ? 'bg-white' : 'bg-[#0f1121]'}`}>
-        {results.map(result => (
-          <SearchResult result={result} key={result.id} />
-        ))}
-      </h1>
+    <div
+      className={`w-full flex flex-col shadow-lg rounded-lg mt-4 max-h-[50vh] overflow-y-auto dark:shadow-orange-700 light:shadow-grey-700 br-10 ${theme === 'light' ? 'bg-white' : 'bg-[#0f1121]'}`}
+      style={{
+        overflowY: 'scroll',
+        scrollbarWidth: 'none',
+        msOverflowStyle: 'none',
+      }}
+    >
+      {results.map(result => (
+        <div
+          key={result.id}
+          className="p-2.5 px-5 hover:bg-slate-400 transition-colors duration-200 cursor-pointer"
+          onClick={e => handleItemClick(result, e)}
+        >
+          <Link to={`/${result.category}/${result.itemId}`} className="block">
+            <h1
+              className={`mt-1 font-medium text-sm leading-5 ${
+                theme === 'light' ? 'text-[#313237]' : 'text-[#F1F2F9]'
+              }`}
+            >
+              {result.name}
+            </h1>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
